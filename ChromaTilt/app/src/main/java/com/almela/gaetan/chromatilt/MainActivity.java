f180b7c236c51562030daa7c88f278e0efed53a9
Will Garrett <willgarrett610@gmail.com>
2017-12-30 14:35:50 -0700

    Image preview kind of works

--- ChromaTilt/app/src/main/java/com/almela/gaetan/chromatilt/MainActivity.java
+++ ChromaTilt/app/src/main/java/com/almela/gaetan/chromatilt/MainActivity.java
@@ -3,8 +3,10 @@ package com.almela.gaetan.chromatilt;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
+import android.graphics.ImageFormat;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.SurfaceTexture;
@@ -16,6 +18,10 @@ import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CameraMetadata;
 import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Handler;
+import android.os.HandlerThread;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -25,7 +31,9 @@ import android.view.Surface;
 import android.view.TextureView;
 import android.view.WindowManager;
 import android.widget.Toast;
+import android.widget.FrameLayout;
 
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -41,7 +49,7 @@ public class MainActivity extends AppCompatActivity {
             new TextureView.SurfaceTextureListener() {
                 @Override
                 public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-                    setupCamera(width, height);
+                    setupCamera(width/2, height/2);
                     openCamera();
                 }
 
@@ -86,34 +94,63 @@ public class MainActivity extends AppCompatActivity {
     private CameraCaptureSession.CaptureCallback mSessionCaptureCallback = new CameraCaptureSession.CaptureCallback() {
         @Override
         public void onCaptureSequenceCompleted(CameraCaptureSession session, int sequenceId, long frameNumber) {
-            Canvas canvas = previewSurface.lockCanvas(new Rect(0,0,mPreviewSize.getWidth(), mPreviewSize.getHeight()));
-            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-            canvas.drawColor(Color.RED);
-            canvas.drawRect(100,100,100,100,paint);
-            previewSurface.unlockCanvasAndPost(canvas);
         }
     };
     Surface previewSurface;
     private static final int CAMERA_REQUEST_CODE = 9843;
+    private HandlerThread mBackgroundThread;
+    private Handler mBackgroundHandler;
+    private ImageReader mImageReader;
+    private final ImageReader.OnImageAvailableListener mOnImageAvailableListener = new ImageReader.OnImageAvailableListener() {
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+            Image image = null;
+            try {
+                image = reader.acquireLatestImage();
+            } catch (IllegalStateException e) {
+                return;
+            }
+            ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+            byte[] bytes = new byte[buffer.capacity()];
+            buffer.get(bytes);
+            Bitmap bImage = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, null);
+            image.close();
 
+            Canvas canvas = previewSurface.lockCanvas(new Rect(0,0,mPreviewSize.getWidth(), mPreviewSize.getHeight()));
+            Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            canvas.drawBitmap(bImage, 0f, 0f, paint);
+            previewSurface.unlockCanvasAndPost(canvas);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mTextureView = (TextureView) findViewById(R.id.textureView);
+        mTextureView.setLayoutParams(new FrameLayout.LayoutParams(mTextureView.getWidth(), (int) ((float) mTextureView.getWidth() * (16/9))));
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        mTextureView = (TextureView) findViewById(R.id.textureView);
     }
 
     @Override
     public void onResume() {
         super.onResume();
 
+        openBackgroundThread();
         if(mTextureView.isAvailable()) {
-
+            setupCamera(mTextureView.getWidth(), mTextureView.getHeight());
+            openCamera();
         } else {
             mTextureView.setSurfaceTextureListener(mSurfaceTextureListener);
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        closeCamera();
+        closeBackgroundThread();
     }
 
     @Override
@@ -138,7 +175,12 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 StreamConfigurationMap map = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+
                 mPreviewSize = getPreferredPreviewSize(map.getOutputSizes(SurfaceTexture.class), width, height);
+
+                mImageReader = ImageReader.newInstance(mPreviewSize.getWidth(), mPreviewSize.getHeight(), ImageFormat.JPEG, 10);
+                mImageReader.setOnImageAvailableListener(mOnImageAvailableListener, mBackgroundHandler);
+
                 mCameraId = camerId;
                 return;
             }
@@ -175,12 +217,27 @@ public class MainActivity extends AppCompatActivity {
         CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
         try {
             try {
-                cameraManager.openCamera(mCameraId, mCameraDeviceStateCallback, null);
+                cameraManager.openCamera(mCameraId, mCameraDeviceStateCallback, mBackgroundHandler);
             } catch (SecurityException e) {
                 ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
             }
         } catch (CameraAccessException e) {
             e.printStackTrace();
+        }
+    }
+
+    private void closeCamera() {
+        if (mCameraCaptureSession != null) {
+            mCameraCaptureSession.close();
+            mCameraCaptureSession = null;
+        }
+        if (mCameraDevice != null) {
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
+        if (mImageReader != null) {
+            mImageReader.close();
+            mImageReader = null;
         }
     }
 
@@ -190,17 +247,17 @@ public class MainActivity extends AppCompatActivity {
             surfaceTexture.setDefaultBufferSize(mPreviewSize.getWidth(), mPreviewSize.getHeight());
             previewSurface = new Surface(surfaceTexture);
             mPreviewCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
-            mPreviewCaptureRequestBuilder.addTarget(previewSurface);
-            mCameraDevice.createCaptureSession(Arrays.asList(previewSurface), new CameraCaptureSession.StateCallback() {
+            mPreviewCaptureRequestBuilder.addTarget(mImageReader.getSurface());
+            mCameraDevice.createCaptureSession(Arrays.asList(mImageReader.getSurface()), new CameraCaptureSession.StateCallback() {
                 @Override
                 public void onConfigured(CameraCaptureSession session) {
                     if (mCameraDevice == null)
                         return;
-
+                    mPreviewCaptureRequest = mPreviewCaptureRequestBuilder.build();
+                    mCameraCaptureSession = session;
                     try {
-                        mPreviewCaptureRequest = mPreviewCaptureRequestBuilder.build();
-                        mCameraCaptureSession = session;
-                        mCameraCaptureSession.setRepeatingRequest(mPreviewCaptureRequest, mSessionCaptureCallback, null);
+                        mCameraCaptureSession.setRepeatingRequest(mPreviewCaptureRequest, mSessionCaptureCallback, mBackgroundHandler);
+                        //mCameraCaptureSession.capture(mPreviewCaptureRequest, mSessionCaptureCallback, mBackgroundHandler);
                     } catch (CameraAccessException e) {
                         e.printStackTrace();
                     }
@@ -212,6 +269,23 @@ public class MainActivity extends AppCompatActivity {
                 }
             }, null);
         } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void openBackgroundThread() {
+        mBackgroundThread = new HandlerThread("Camera2 background thread");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+    }
+
+    private void closeBackgroundThread() {
+        mBackgroundThread.quitSafely();
+        try {
+            mBackgroundThread.join();
+            mBackgroundThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
