948e34be983293ba40acc7b2fc2e10ee70a549c5
Will Garrett <willgarrett610@gmail.com>
2018-01-11 21:52:54 -0700

    OpenGL Implemented

--- ChromaTilt/app/src/main/java/com/almela/gaetan/chromatilt/GLRenderer.java
+++ ChromaTilt/app/src/main/java/com/almela/gaetan/chromatilt/GLRenderer.java
@@ -2,15 +2,20 @@ package com.almela.gaetan.chromatilt;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.ColorSpace;
+import android.media.Image;
+import android.media.ImageReader;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.GLUtils;
 import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -19,107 +24,174 @@ import javax.microedition.khronos.opengles.GL10;
  * Created by Willg on 1/4/2018.
  */
 
-public class GLRenderer implements GLSurfaceView.Renderer {
+public class GLRenderer implements GLSurfaceView.Renderer, ImageReader.OnImageAvailableListener {
 
-    private FloatBuffer vertexBuffer;
-    private float vertices[] = {
-            -1f, -1f, 0f,
-            -1f, 1f, 0f,
-            1f, -1f, 0f,
-            -1f, 1f, 0f,
-            1f, 1f, 0f,
-            1f, -1f, 0f
-    };
-    private float color[] = new float[] { 0.0f, 0.6f, 1.0f, 1.0f };
+    private int mProgramObject;
 
-    private int textures[] = new int[1];
-    private float textureCoordinates[] = {0.0f, 0.5f,
-            0.5f, 0.5f,
-            0.0f, 0.0f,
-            0.5f, 0.0f };
-    private FloatBuffer textureBuffer;
+    private int mPositionLoc;
+    private int mTexCoordLoc;
 
-    private final String vertexShaderCode =
-            "attribute vec4 vPosition;" +
-            "void main() {" +
-            "   gl_Position = vPosition;" +
-            "}";
-    private final String fragmentShaderCode =
-            "precision mediump float;" +
-            "uniform vec4 vColor;" +
-            "void main() {" +
-            "   gl_FragColor = vColor;" +
-            "}";
-    private int shaderProgram;
+    private int mSamplerLoc;
 
-    public static int loadShader(int type, String shaderCode) {
-        int shader = GLES20.glCreateShader(type);
-        GLES20.glShaderSource(shader, shaderCode);
-        GLES20.glCompileShader(shader);
-        return shader;
+    private int mTextureId = -1;
+
+    private int mWidth;
+    private int mHeight;
+    private FloatBuffer mVertices;
+    private ShortBuffer mIndices;
+
+    private float[] mVerticesData;
+
+    private final short[] mIndicesData =
+            {
+                    0, 1, 2, 0, 2, 3
+            };
+
+    Bitmap cameraImage;
+
+    public void setAspectRatio(DisplayMetrics displayMetrics, float aspectRatio) {
+        float xOff = ((float)displayMetrics.heightPixels * aspectRatio) / (float) displayMetrics.widthPixels;
+        mVerticesData =
+                new float[] {
+                        -xOff, 1f, 0.0f,
+                        0.0f, 0.0f,
+                        -xOff, -1f, 0.0f,
+                        0.0f, 1.0f,
+                        xOff, -1f, 0.0f,
+                        1.0f, 1.0f,
+                        xOff, 1f, 0.0f,
+                        1.0f, 0.0f
+                };
+        mVertices = ByteBuffer.allocateDirect(mVerticesData.length * 4)
+                .order(ByteOrder.nativeOrder()).asFloatBuffer();
+        mVertices.put(mVerticesData).position(0);
+        mIndices = ByteBuffer.allocateDirect(mIndicesData.length * 2)
+                .order(ByteOrder.nativeOrder()).asShortBuffer();
+        mIndices.put(mIndicesData).position(0);
+
+        cameraImage = Bitmap.createBitmap((DisplayMetrics) null, 5, 5, Bitmap.Config.ARGB_8888);
     }
 
     @Override
-    public void onDrawFrame(GL10 gl) {
-        GLES20.glUseProgram(shaderProgram);
-
-        int positionAttrib = GLES20.glGetAttribLocation(shaderProgram, "vPosition");
-        GLES20.glEnableVertexAttribArray(positionAttrib);
-
-        GLES20.glVertexAttribPointer(positionAttrib, 3, GLES20.GL_FLOAT, false, 0, vertexBuffer);
-
-        int colorUniform = GLES20.glGetUniformLocation(shaderProgram, "vColor");
-
-        GLES20.glUniform4fv(colorUniform, 1, color, 0);
-        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertices.length / 3);
-        GLES20.glDisableVertexAttribArray(positionAttrib);
-    }
-
-    @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        ByteBuffer bb = ByteBuffer.allocateDirect(vertices.length * 4);
-        bb.order(ByteOrder.nativeOrder());
-
-        vertexBuffer = bb.asFloatBuffer();
-        vertexBuffer.put(vertices);
-        vertexBuffer.position(0);
-
-        int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, vertexShaderCode);
-        int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
-
-        shaderProgram = GLES20.glCreateProgram();
-        GLES20.glAttachShader(shaderProgram, vertexShader);
-        GLES20.glAttachShader(shaderProgram, fragmentShader);
-        GLES20.glLinkProgram(shaderProgram);
-
-        Bitmap bitmap = Bitmap.createBitmap((DisplayMetrics) null, 100,100, Bitmap.Config.ARGB_8888);
-        for (int x = 0; x < bitmap.getWidth(); x++) {
-            for (int y = 0; y < bitmap.getHeight(); y++) {
-                bitmap.setPixel(x,y,(int) (Math.random() * Math.pow(2,32)));
-            }
+    public void onImageAvailable(ImageReader reader) {
+        Image image;
+        try {
+            image = reader.acquireLatestImage();
+        } catch (IllegalStateException e) {
+            return;
         }
-
-        GLES20.glGenTextures(1, textures, 0);
-        GLES20.glBindTexture(GL10.GL_TEXTURE_2D, textures[0]);
-        GLES20.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MAG_FILTER, GL10.GL_LINEAR);
-        GLES20.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_MIN_FILTER, GL10.GL_LINEAR);
-        GLES20.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_S, GL10.GL_REPEAT);
-        GLES20.glTexParameterf(GL10.GL_TEXTURE_2D, GL10.GL_TEXTURE_WRAP_T, GL10.GL_REPEAT);
-
-        GLUtils.texImage2D(GL10.GL_TEXTURE_2D, 0, bitmap, 0);
-
-        bitmap.recycle();
-
-        ByteBuffer byteBuf = ByteBuffer.allocateDirect(textureCoordinates.length * 4);
-        byteBuf.order(ByteOrder.nativeOrder());
-        textureBuffer = byteBuf.asFloatBuffer();
-        textureBuffer.put(textureCoordinates);
-        textureBuffer.position(0);
+        if(image != null) {
+            ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+            byte[] bytes = new byte[buffer.capacity()];
+            buffer.get(bytes);
+            cameraImage = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, null);
+            image.close();
+        }
     }
 
-    @Override
-    public void onSurfaceChanged(GL10 gl, int width, int height) {
+    public void createTexture()
+    {
+        if (mTextureId != -1) {
+            GLES20.glDeleteTextures(1, new int[] {mTextureId}, 0);
+        }
+        int[] textureId = new int[1];
 
+        GLES20.glPixelStorei ( GLES20.GL_UNPACK_ALIGNMENT, 1 );
+
+        GLES20.glGenTextures ( 1, textureId, 0 );
+
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureId[0]);
+
+        GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, cameraImage, 0);
+
+        GLES20.glTexParameteri ( GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST );
+        GLES20.glTexParameteri ( GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST );
+
+        mTextureId = textureId[0];
+    }
+
+    public void onSurfaceCreated(GL10 glUnused, EGLConfig config)
+    {
+        String vShaderStr =
+                "attribute vec4 a_position;   \n"
+                        + "attribute vec2 a_texCoord;   \n"
+                        + "varying vec2 v_texCoord;     \n"
+                        + "void main()                  \n"
+                        + "{                            \n"
+                        + "   gl_Position = a_position; \n"
+                        + "   v_texCoord = a_texCoord;  \n"
+                        + "}                            \n";
+
+        String fShaderStr =
+                "precision mediump float;                            \n"
+                        + "varying vec2 v_texCoord;                            \n"
+                        + "uniform sampler2D s_texture;                        \n"
+                        + "void main()                                         \n"
+                        + "{                                                   \n"
+                        + "  gl_FragColor = texture2D( s_texture, v_texCoord );\n"
+                        + "}                                                   \n";
+
+        int vShader = GLES20.glCreateShader(GLES20.GL_VERTEX_SHADER);
+        GLES20.glShaderSource(vShader, vShaderStr);
+        GLES20.glCompileShader(vShader);
+
+        int fShader = GLES20.glCreateShader(GLES20.GL_FRAGMENT_SHADER);
+        GLES20.glShaderSource(fShader, fShaderStr);
+        GLES20.glCompileShader(fShader);
+
+        mProgramObject = GLES20.glCreateProgram();
+
+        GLES20.glAttachShader(mProgramObject, vShader);
+        GLES20.glAttachShader(mProgramObject, fShader);
+
+        GLES20.glLinkProgram(mProgramObject);
+
+        GLES20.glDeleteShader(vShader);
+        GLES20.glDeleteShader(fShader);
+
+        mPositionLoc = GLES20.glGetAttribLocation(mProgramObject, "a_position");
+        mTexCoordLoc = GLES20.glGetAttribLocation(mProgramObject, "a_texCoord");
+
+        mSamplerLoc = GLES20.glGetUniformLocation(mProgramObject, "s_texture");
+
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+    }
+
+    public void onDrawFrame(GL10 glUnused)
+    {
+        createTexture();
+
+        GLES20.glViewport(0, 0, mWidth, mHeight);
+
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        GLES20.glUseProgram(mProgramObject);
+
+        mVertices.position(0);
+        GLES20.glVertexAttribPointer ( mPositionLoc, 3, GLES20.GL_FLOAT,
+                false,
+                5 * 4, mVertices );
+        mVertices.position(3);
+        GLES20.glVertexAttribPointer ( mTexCoordLoc, 2, GLES20.GL_FLOAT,
+                false,
+                5 * 4,
+                mVertices );
+
+        GLES20.glEnableVertexAttribArray ( mPositionLoc );
+        GLES20.glEnableVertexAttribArray ( mTexCoordLoc );
+
+        GLES20.glActiveTexture ( GLES20.GL_TEXTURE0 );
+        GLES20.glBindTexture ( GLES20.GL_TEXTURE_2D, mTextureId );
+
+        GLES20.glUniform1i ( mSamplerLoc, 0 );
+
+        GLES20.glDrawElements ( GLES20.GL_TRIANGLES, 6, GLES20.GL_UNSIGNED_SHORT, mIndices );
+    }
+
+    public void onSurfaceChanged(GL10 glUnused, int width, int height)
+    {
+        mWidth = width;
+        mHeight = height;
     }
 
 }
--- ChromaTilt/app/src/main/java/com/almela/gaetan/chromatilt/MainActivity.java
+++ ChromaTilt/app/src/main/java/com/almela/gaetan/chromatilt/MainActivity.java
@@ -1,27 +1,252 @@
 package com.almela.gaetan.chromatilt;
 
-import android.app.ActivityManager;
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.ImageFormat;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.Size;
+import android.view.Surface;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Toast;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private FilterGLSurfaceView mSurfaceView;
+    private GLRenderer renderer;
+    private Size mPreviewSize;
+    private String mCameraId;
+    private CameraDevice mCameraDevice;
+    private CameraDevice.StateCallback mCameraDeviceStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(CameraDevice camera) {
+            mCameraDevice = camera;
+            createCameraPreviewSession();
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            camera.close();
+            mCameraDevice = null;
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {
+            camera.close();
+            mCameraDevice = null;
+        }
+    };
+    private CaptureRequest mPreviewCaptureRequest;
+    private CaptureRequest.Builder mPreviewCaptureRequestBuilder;
+    private CameraCaptureSession mCameraCaptureSession;
+    private CameraCaptureSession.CaptureCallback mSessionCaptureCallback = new CameraCaptureSession.CaptureCallback() {
+        @Override
+        public void onCaptureSequenceCompleted(CameraCaptureSession session, int sequenceId, long frameNumber) {
+        }
+    };
+    private static final int CAMERA_REQUEST_CODE = 9843;
+    private HandlerThread mBackgroundThread;
+    private Handler mBackgroundHandler;
+    private ImageReader mImageReader;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityManager am = (ActivityManager)getSystemService(Context.ACTIVITY_SERVICE);
-        GLRenderer renderer = new GLRenderer();
+        renderer = new GLRenderer();
         mSurfaceView = new FilterGLSurfaceView(this);
         mSurfaceView.setEGLContextClientVersion(2);
         mSurfaceView.setRenderer(renderer);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.setContentView(mSurfaceView);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        openBackgroundThread();
+
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        setupCamera(displayMetrics.widthPixels / 2, displayMetrics.heightPixels / 2);
+        openCamera();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        closeCamera();
+        closeBackgroundThread();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                openCamera();
+            else
+                Toast.makeText(this, "Camera permission denied", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void setupCamera(int width, int height) {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            for (String camerId : cameraManager.getCameraIdList()) {
+                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(camerId);
+                if (cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) == CameraMetadata.LENS_FACING_FRONT) {
+                    continue;
+                }
+
+                StreamConfigurationMap map = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+
+                mPreviewSize = getPreferredPreviewSize(map.getOutputSizes(SurfaceTexture.class), width, height);
+
+                DisplayMetrics displayMetrics = new DisplayMetrics();
+                getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+                renderer.setAspectRatio(displayMetrics, (float) mPreviewSize.getWidth() / (float) mPreviewSize.getHeight());
+
+                mImageReader = ImageReader.newInstance(mPreviewSize.getWidth(), mPreviewSize.getHeight(), ImageFormat.JPEG, 10);
+                mImageReader.setOnImageAvailableListener(renderer, mBackgroundHandler);
+
+                mCameraId = camerId;
+                return;
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Size getPreferredPreviewSize(Size[] mapSizes, int width, int height) {
+        List<Size> collectorSizes = new ArrayList<>();
+        for (Size option : mapSizes) {
+            if (width > height) {
+                if (option.getWidth() > width && option.getHeight() > height) {
+                    collectorSizes.add(option);
+                }
+            } else {
+                if (option.getWidth() > height && option.getHeight() > width) {
+                    collectorSizes.add(option);
+                }
+            }
+        }
+        if (collectorSizes.size() > 0) {
+            return Collections.min(collectorSizes, new Comparator<Size>() {
+                @Override
+                public int compare(Size o1, Size o2) {
+                    return Long.signum(o1.getWidth() * o1.getHeight() - o2.getWidth() * o2.getHeight());
+                }
+            });
+        }
+        return mapSizes[0];
+    }
+
+    private void openCamera() {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            try {
+                cameraManager.openCamera(mCameraId, mCameraDeviceStateCallback, mBackgroundHandler);
+            } catch (SecurityException e) {
+                ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+            }
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void closeCamera() {
+        if (mCameraCaptureSession != null) {
+            mCameraCaptureSession.close();
+            mCameraCaptureSession = null;
+        }
+        if (mCameraDevice != null) {
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
+        if (mImageReader != null) {
+            mImageReader.close();
+            mImageReader = null;
+        }
+    }
+
+    private void createCameraPreviewSession() {
+        try {
+            mPreviewCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            mPreviewCaptureRequestBuilder.addTarget(mImageReader.getSurface());
+            mCameraDevice.createCaptureSession(Arrays.asList(mImageReader.getSurface()), new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(CameraCaptureSession session) {
+                    if (mCameraDevice == null)
+                        return;
+                    mPreviewCaptureRequest = mPreviewCaptureRequestBuilder.build();
+                    mCameraCaptureSession = session;
+                    try {
+                        mCameraCaptureSession.setRepeatingRequest(mPreviewCaptureRequest, mSessionCaptureCallback, mBackgroundHandler);
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(CameraCaptureSession session) {
+                    Toast.makeText(getApplicationContext(), "Failed to create camera session!", Toast.LENGTH_SHORT).show();
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void openBackgroundThread() {
+        mBackgroundThread = new HandlerThread("Camera2 background thread");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
+    }
+
+    private void closeBackgroundThread() {
+        mBackgroundThread.quitSafely();
+        try {
+            mBackgroundThread.join();
+            mBackgroundThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
